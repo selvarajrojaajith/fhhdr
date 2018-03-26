57e98d4cbb3e11afa73cb1c63007d5eebc02e991
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-26 15:49:22 +0530

    string.h
    good

---
+++ string.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+int main ( int argc , char* argv[])
+{
+char* s = "CPROGRAMMING";
+int i;
+for(i=0; , i< strlen; i++ )
+{
+   if( isalpha(s[i]) && isupper(s[i]) )
+     flag = true;
+   else
+     flag = false;
+}
+if (flag)
+     printf("A string contains and alphabet letters");
+    
+return 0;
+}
